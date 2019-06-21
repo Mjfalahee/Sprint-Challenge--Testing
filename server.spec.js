a226788532d138bc63bfdd1b5ddf690b4bd215a8
mjfalahee <mjfalahee@gmail.com>
2019-06-21 10:39:02 -0700

    Got the MVP done, except post test case fails on second run because of duplicate title

--- server.spec.js
+++ server.spec.js
@@ -2,26 +2,6 @@ const supertest = require('supertest');
 const server = require('./server');
 require('jest-extended');
 
-const seeddata = [
-    {id: 1, title: 'Pac Man', genre: 'Arcade', releaseYear: '1980'},
-    {id: 2, title: 'Doom', genre: 'FPS', releaseYear: '1993'},
-    {id: 3, title: 'Diablo', genre: 'ARPG', releaseYear: '1996'}
-  ]
-//minimum 3 tests per endpoint
-
-//the POST /games should take in an object 
-// obj = {
-//     title: 'Pacman', required
-//     genre: 'Arcade', required
-//     releaseYear: 1980 not required
-// }
-
-//In the route handler validate that the required fields are included inside the body. If the information is incomplete, return a 422 status code.
-//Test -- correct status, receiving correct/incorrect game data
-
-//GET /games should return a list of games, and status code 200
-//write a test to make sure the endpoint returns an array, even if no games are stored. If there are no games to return, the endpoint should return an empty array. 
-
 describe('server', () => {
     describe('get /games', () => {
         it('returns with status code 200, OK', () => {
